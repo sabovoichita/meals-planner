daa790569eb3a2925ab125fc0ac6a8ba846e8856
Voichita Maria <sabo_voichita@yahoo.com>
2024-03-20 11:55:28 +0100

    render meals (add getMealsAsHTML(meal) and renderMeals(meals))

--- SRC/index.js
+++ SRC/index.js
@@ -1,10 +1,28 @@
 import("./style.css");
 
+function getMealAsHTML(meal) {
+  //   console.info("inside map");
+  return `<tr>
+  <td>${meal.order}</td>
+  <td>${new Date().toLocaleString()}</td>
+  <td>${meal.meal}</td>
+  <td>${meal.symptom}</td>
+  <td>${meal.avoid}</td>
+  <td><span class="plus">&#43;</span></td>
+</tr>`;
+}
+
+function renderMeals(meals) {
+  const mealsHTML = meals.map(getMealAsHTML);
+  document.querySelector("#mealsTable tbody").innerHTML = mealsHTML.join("");
+}
+
 function loadMeals() {
   fetch("meals.json")
-    .then(r => r.json)
+    .then(r => r.json())
     .then(meals => {
-      console.warn("meals", meals);
+      renderMeals(meals);
     });
 }
+
 loadMeals();
